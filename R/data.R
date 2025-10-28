07a8fdeb3953e9ea9914c698886bbedc7ad5a556
sigridfrandsen <frandsensigrid@gmail.com>
2025-10-28 10:07:33 +0100

    Task 2.1

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' AA codon Table
+#'
+#' standard amino acid codon table
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codon_table"
+
+
+
